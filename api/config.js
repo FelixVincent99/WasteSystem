05a8872e7b344cf8113bcb06b4c18036f9a1e597
FelixVincent12 <felixvincent9933@gmail.com>
2022-08-07 23:42:53 +0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 var config = mysql.createConnection({
-    host: "10.40.120.37",
+    host: "10.40.120.36",
     user: "root",
     password: "SkhFvLsh22*",
     database: "dbtssmartsystem"
