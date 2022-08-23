e5d980e67376fe13fce497c3748d6819fe722502
FelixVincent12 <felixvincent9933@gmail.com>
2022-08-23 14:19:30 +0700

    Initial Web Design

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ app.use('/api', router);
 const sensor = require('./api/sensor_api');
 
 app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'assets')));
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('index'));
 
-app.route('/send_from_sensor').get((request, res) => {
+router.route('/send_from_sensor').get((request, res) => {
     var data = url.parse(request.url, true).query;
     console.log(data);
     sensor.sendData(data).then(result => {
