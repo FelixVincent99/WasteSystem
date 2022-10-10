e3aa6b3bd4499ac6fa7352da020efccdad012a7f
FelixVincent12 <felixvincent9933@gmail.com>
2022-10-10 11:09:26 +0700

    Update db.config.js
    change localhost to ip

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "SkhFvLsh22*",
-  DB: "dbtssmartsystem",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+    HOST: "219.93.5.3",
+    USER: "root",
+    PASSWORD: "SkhFvLsh22*",
+    DB: "dbtssmartsystem",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 };
