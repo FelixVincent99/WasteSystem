e2945fe79b339fbd18099b03a1b516eb189ba5ee
lshong98 <lshong9899@gmail.com>
2022-10-16 18:47:41 +0800

    fix bug for auth

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const token = JSON.parse(localStorage.getItem("user")) ? JSON.parse(localStorage.getItem("user")).token : '';
+
 export default axios.create({
   baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json", 
-    "Authorization": `Bearer ${JSON.parse(localStorage.getItem("user")).token}`
+    "Authorization": `Bearer ${token}`
   }
 });
