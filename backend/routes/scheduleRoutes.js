4ca30b20b814264cf08a963838f3967d09615997
FelixVincent12 <felixvincent9933@gmail.com>
2022-10-17 15:00:39 +0700

    Update scheduleRoutes.js
    add protect on api

--- backend/routes/scheduleRoutes.js
+++ backend/routes/scheduleRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const scheduleController = require('../controllers/scheduleController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.post('/', scheduleController.addSchedule);
-router.get('/', scheduleController.getAllSchedules);
-router.get('/:id', scheduleController.getOneSchedule);
-router.put('/:id', scheduleController.updateSchedule);
+router.post('/', protect, scheduleController.addSchedule);
+router.get('/', protect, scheduleController.getAllSchedules);
+router.get('/:id', protect, scheduleController.getOneSchedule);
+router.put('/:id', protect, scheduleController.updateSchedule);
 
 module.exports = router;
