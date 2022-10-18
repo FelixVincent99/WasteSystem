ab091cfa291fe74ffd560e13242d17ff7d0df2b8
lshong98 <lshong9899@gmail.com>
2022-10-18 13:57:39 +0800

    navigate to manpower list after created

--- frontend/src/pages/Manpower/AddManpower.jsx
+++ frontend/src/pages/Manpower/AddManpower.jsx
@@ -40,7 +40,7 @@ function AddManpower() {
         }
         if(isSuccess){
             toast.success("Manpower has been created")
-            // navigate('/manpowerlist')
+            navigate('/manpowerlist')
         }
 
         dispatch(reset())
